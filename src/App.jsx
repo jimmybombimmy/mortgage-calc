637fb1a86ff8f2148a49c5f75f37d5fe8d908ac8
James Peters <jamespeters92@gmail.com>
2024-12-02 09:58:16 +0000

    add styling with all information logged on form submit

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
     const mortgageDetails = {
       loanAmount: convertGBPToNum(amount),
       annualInterestRate: interest,
-      loanTermMonths: (Number(term[0] * 12) + Number(term[1])),
+      loanTermMonths: Number(term[0] * 12) + Number(term[1]),
     };
     e.preventDefault();
     calculateSavings(mortgageDetails, convertGBPToNum(overpayment));
@@ -23,41 +23,67 @@ export default function App() {
 
   return (
     <main>
-      <h1>Mortgage Calulcator</h1>
+      <h1>Mortgage Overpayment Savings Calulcator</h1>
       <h2></h2>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="amount">Loan amount - </label>
-        <input
-          type="text"
-          name="amount"
-          value={amount}
-          onChange={(e) => convertNumToGBP(e.target.value, setAmount)}
-        />
+        <div className="label-inner">
+          <label htmlFor="amount">
+            <strong>Loan amount</strong> -{" "}
+          </label>
+          <input
+            type="text"
+            name="amount"
+            value={amount}
+            onChange={(e) => convertNumToGBP(e.target.value, setAmount)}
+          />
+        </div>
         <br />
-        <label htmlFor="interest">Interest Rate (APR) - </label>
-        <input
-          type="number"
-          name="interest"
-          defaultValue={interest}
-          step="0.1"
-        />
-        %
+        <div className="label-inner">
+          <label htmlFor="interest">
+            <strong>Interest Rate (APR %) </strong> -{" "}
+          </label>
+          <input
+            type="number"
+            name="interest"
+            defaultValue={interest}
+            step="0.1"
+          />
+        </div>
         <br />
-        Term - 
-        <label htmlFor="termYears"> Years: </label>
-        <input type="number" name="termYears" defaultValue={term[0]} onChange={(e) => setTerm([e.target.value, term[1]])} />
-        <label htmlFor="termMonths"> Months: </label>
-        <input type="number" name="termMonths" defaultValue={term[1]} onChange={(e) => setTerm([term[0], e.target.value])}/>
+        <p className="term-title">
+          <strong>Term</strong> -{" "}
+        </p>
+        <div className="term-inner">
+          <label htmlFor="termYears"> Years: </label>
+          <input
+            type="number"
+            name="termYears"
+            defaultValue={term[0]}
+            onChange={(e) => setTerm([e.target.value, term[1]])}
+          />
+        </div>
+        <div className="term-inner">
+          <label htmlFor="termMonths"> Months: </label>
+          <input
+            type="number"
+            name="termMonths"
+            defaultValue={term[1]}
+            onChange={(e) => setTerm([term[0], e.target.value])}
+          />
+        </div>
+        <div className="label-inner">
+          <label htmlFor="overpayment">
+            <strong>Overpayment amount</strong> -{" "}
+          </label>
+          <input
+            type="text"
+            name="overpayment"
+            value={overpayment}
+            onChange={(e) => convertNumToGBP(e.target.value, setOverpayment)}
+          />
+        </div>
         <br />
-        <label htmlFor="overpayment">Overpayment amount - </label>
-        <input
-          type="text"
-          name="overpayment"
-          value={overpayment}
-          onChange={(e) => convertNumToGBP(e.target.value, setOverpayment)}
-        />
-        <br />
-        <input type="submit" />
+        <button className="form-button" type="submit">Submit</button>
       </form>
     </main>
   );
--- src/utils/calculateSavings.js
+++ src/utils/calculateSavings.js
@@ -2,12 +2,13 @@ import calculateMortgagePayments from "./calculateMortgagePayment"
 
 export default function calculateSavings(mortgageInfo, overpayment) {
   const preOverpaymentTable = calculateMortgagePayments(mortgageInfo.loanAmount, mortgageInfo.annualInterestRate, mortgageInfo.loanTermMonths)
-  console.log(preOverpaymentTable)
+  console.log("Pre Overpayment Table", preOverpaymentTable)
 
   const newLoanAmount = mortgageInfo.loanAmount - overpayment
   const preOverpaymentMonthlyPayment = preOverpaymentTable[0].totalPayment
 
   const postOverpaymentTable = calculateMortgagePayments(newLoanAmount, mortgageInfo.annualInterestRate, mortgageInfo.loanTermMonths, preOverpaymentMonthlyPayment)
+  console.log("Post Overpayment Table", postOverpaymentTable)
 
   const savingsCalculated = []
   let totalInterestSaved = 0
